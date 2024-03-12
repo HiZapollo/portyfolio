98a0f021b2c52e08382704c3adbabe7d3fdd2ccc
zeckenrode <zeckenrode9000@gmail.com>
2024-03-12 14:41:36 -0400

    More advanced hamburger icon with transitions. Positioned properly to follow scroll.

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
+let menu = document.querySelector('.hamburger');
+let sideMenu = document.querySelector('.side-menu');
+
 function openSideMenu() {
-    document.getElementById("sideMenu").style.width = "250px";
+    // document.getElementById("sideMenu").style.width = "250px";
+    menu.classList.toggle('change');
+    sideMenu.classList.toggle('open');
 }
 
 function closeSideMenu() {
-    document.getElementById("sideMenu").style.width = "0";
+    // document.getElementById("sideMenu").style.width = "0";
+    menu.classList.toggle('change');
+    sideMenu.classList.toggle('open');
 }
